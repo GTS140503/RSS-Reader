0815009df6ba6bee95b62d469e6bbcbac35d48b8
OTAKU-G <gt689229@gmail.com>
2014-05-04 03:08:57 +0800

    1. Bootstrap 引用 2. 基本版面完成

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WEB RSS Reader</title>
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/bootstrap-theme.css"/>
+    <link rel="stylesheet" href="css/custom.css"/>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-default">
+            <a href="#" class="navbar-brand">WEB RSS Reader</a>
+            <form class="navbar-form navbar-right" action="">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Username"/>
+                    <input type="password" class="form-control" placeholder="Password"/>
+                    <button class="btn btn-default">Log In</button>
+                </div>
+            </form>
+        </nav>
+    </header>
+    <aside class="list-group">
+        <div class="list-group-item active">
+            <span>豬排！</span>
+            <button class="btn btn-danger">刪除</button>
+        </div>
+        <div class="list-group-item">
+            <span>妖精之森</span>
+        </div>
+        <div class="list-group-item">
+            <span>Jobs 不要偷載片</span>
+        </div>
+    </aside>
+    <section>
+        <div class="item read clearfix">
+            <div class="title"><h2>為什麼摔跤的時候要大喊一聲「豬排」？</h2></div>
+            <div class="description">這是個令人深思的哲學問題……</div>
+            <div class="control">
+                <button class="btn btn-primary">閱讀文章</button>
+                <button class="btn btn-success">標示已讀</button>
+            </div>
+        </div>
+        <div class="item clearfix">
+            <div class="title"><h2>好想吃豬排飯！</h2></div>
+            <div class="description">好想吃樹太老的豬排飯！</div>
+            <div class="control">
+                <button class="btn btn-primary">閱讀文章</button>
+            </div>
+        </div>
+        <div class="item clearfix">
+            <div class="title"><h2>明天回營哭哭 QQQQQ</h2></div>
+            <div class="description">1800 收假，1530 就要出門了。</div>
+            <div class="control">
+                <button class="btn btn-primary">閱讀文章</button>
+            </div>
+        </div>
+    </section>
+    <footer>
+    </footer>
+</body>
+</html>
