c887927bafb359751e44594bdd3be5081c36486e
TangSong <jim20259@gmail.com>
2014-05-27 13:16:03 +0800

    新增 config.php 、db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<? php
+    $public_dbc;
+    function db_connection($host,$username,$password,$db_name){
+        $public_dbc=mysqli_connect($host,$username,$password,$db_name);
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+    }
+
+    function db_close_connection(){
+        mysqli_close($public_dbc);
+    }
+?>
