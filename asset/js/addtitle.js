632e3fbbe4cab4a73e5ceb31fb7a7dcba46dd420
function <function86437@gmail.com>
2014-05-29 21:42:59 +0800

    addtitle.js completed instead of localstorage

--- asset/js/addtitle.js
+++ asset/js/addtitle.js
@@ -1,4 +1,6 @@
 //FIXME 2014/05/27 set in localstorage
+
+//global counter
 count = 0;
 
 isselected = false;
@@ -9,8 +11,6 @@ selectedid = null;
 var itemtype = {
     "id":null,
     "title":null,
-    "selected":false
-
 };
 
 var itemarray = [];
@@ -20,22 +20,12 @@ var itemarray = [];
 * Add a new item (url)
 * @param title title which is connected by url is named by user
 **/
-
 var additem = function(title){
     var item = document.createElement('div');
-
-    itemarray.push({
-        "id":count,
-        "title":title,
-        "selected":false
-    });
-
     //create id
     $(item).attr('id',count);
     //create current id number
     var current = $(item).attr('id');
-    console.log('current ' + current);
-
     $(item).addClass('list-group-item');
 
     item.addEventListener('click',function(){
@@ -46,10 +36,16 @@ var additem = function(title){
 
     var itemtitle = document.createElement('span');
     itemtitle.appendChild(document.createTextNode(title));
-
     //FIXME 2014/05/27 set onclick event
-
     document.getElementById('rsslist').appendChild(item).appendChild(itemtitle);
+
+    itemarray.push({
+        "id":count,
+        "title":title,
+        "selected":false
+    });
+    console.log(itemarray);
+
     count ++;
 }
 
@@ -63,27 +59,50 @@ var itemseleted = function(id){
 
     if(isselected == true){
         var item = document.getElementById(selectedid);
+
         $(item).attr('class','list-group-item');
+
         //END 2014/05/27
-        $('div.' + selectedid + ' > button').remove();
+        $('div#' + selectedid + ' > button').remove();
 
 
     }
-        isselected = true;
-        selectedid = id;
+    isselected = true;
+    selectedid = id;
 
-        var item = document.getElementById(id);
+    var item = document.getElementById(id);
 
-        //jquery change class
-        $(item).attr("class",'list-group-item active');
-        var itemdeleted = document.createElement('button');
-        itemdeleted.appendChild(document.createTextNode('刪除'));
-        $(itemdeleted).addClass('btn btn-danger');
-        itemdeleted.addEventListener('click',function(){
+    //jquery change class
+    $(item).attr("class",'list-group-item active');
+    var itemdeleted = document.createElement('button');
+    itemdeleted.appendChild(document.createTextNode('刪除'));
+    $(itemdeleted).addClass('btn btn-danger');
+    itemdeleted.addEventListener('click',function(){
 
-            //remove element
-            $("#"+id).remove();
-        },false);
-        document.getElementById(id).appendChild(itemdeleted);
+        //remove element
+        $("#"+id).remove();
+        //remove index of array
+        itemarray.splice(deleteFromArray(id),1);
+        //remove array element
 
+
+    },false);
+
+    document.getElementById(id).appendChild(itemdeleted);
+
+
+}
+
+/**
+*In order to finding index of array,we use value of id.
+*@params valueOfId value of id
+*@return index of array
+**/
+var deleteFromArray = function(valueOfId){
+    for(var i = 0 ; i < itemarray.length ; i ++ ){
+        if(valueOfId == itemarray[i].id){
+            return i;
+        }
+        return -1;
+    }
 }
