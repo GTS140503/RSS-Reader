91483a9a9bc3df4f3a8dd87b56acbb11d5b5308f
TangSong <jim20259@gmail.com>
2014-05-29 12:09:41 +0800

    password使用md5加密，增加註解及TODO

--- f_db.php
+++ f_db.php
@@ -1,14 +1,11 @@
 <?php
     require_once('config.php');
+    //$aaa=new database;
 
-    $aaa=new database;
-    $aaa->getURL(1);
-    $aaa->newUser('admin2','81dc9bdb52d04dc20036dbd8313ed055','email@email.com');
     class database{
         protected $db;
 
         public function __Construct(){
-
         $conn = NULL;
         $dsn = DB_TYPE.':host=' . HOST . ';dbname=' . DB_NAME;
             try{
@@ -24,17 +21,52 @@
             return $this->db;
         }
 
+        #TODO: 確認這功能有沒有需要
         public function dbQuery($query){
             $arr = array();
             $arr[] = $this->db->query($query);
             return $arr;
         }
 
+        /**
+         * 取得某帳號的所有網址
+         * echo 至前端之javascript變數
+         *
+         * @param int $user_id 使用者編號
+         * @return void
+         */
         public function getURL($user_id){
-            $query="SELECT URL,username FROM user_url WHERE ID = $user_id";
+            $query="SELECT URL FROM user_url WHERE ID = ?";
+            $sth = $this->db->query($query);
+            $sth->execute( $user_id );
             $i=0;
             echo '<script>';
-            foreach($this->db->query($query) as $row){
+            foreach( $sth as $row ){
+                #TODO: 修改javascript變數名稱
+                echo 'var i' . $i . ' = \'' . $row['URL'] . '\';';
+                $i++;
+            }
+            echo '</script>';
+        }
+        #NOTE 最後工作位置
+        #NOTE 工作中止原因：修改Master README需切換branch
+        #NOTE 預計下次工作內容：getUserName
+
+        /**
+         * 取得某帳號的所有網站名稱
+         * echo 至前端之javascript變數
+         *
+         * @param int $user_id 使用者編號
+         * @return void
+         */
+        public function getSiteName($user_id){
+            $query="SELECT site_name FROM user_url WHERE ID = ?";
+            $sth = $this->db->query($query);
+            $sth->execute( $user_id );
+            $i=0;
+            echo '<script>';
+            foreach( $sth as $row ){
+                #TODO: 修改javascript變數名稱
                 echo 'var i' . $i . ' = \'' . $row['URL'] . '\';';
                 $i++;
             }
@@ -42,26 +74,36 @@
             echo '</script>';
         }
 
+        /**
+         * 新增使用者
+         *
+         * @param string $username 使用者帳號
+         * @param string $password 使用者密碼
+         * @param string $email 電子信箱
+         * @return boolean 是否新增成功
+         */
         public function newUser($username,$password,$email){
             if($this->checkUser($username)){
-
                 return false;
             }else{
+                $password = md5($password);
                 $query = "INSERT INTO Account (username,password,email) VALUES (?,?,?)";
                 $sth = $this->db->prepare($query);
                 $sth->execute( array($username, $password, $email) );
-
                 return true;
             }
         }
 
+        /**
+         * 檢查資料庫中是否存在傳入的帳號名稱
+         *
+         * @param string $username 使用者帳號
+         * @return boolean 帳號名稱是否存在
+         */
         public function checkUser($username){
-
             $query = "SELECT username FROM Account WHERE username =?";
-
             $sth = $this->db->prepare($query);
-
-            $sth->execute( array($username) );
+            $sth->execute( $username );
             foreach($sth as $row){
                 if($row['username'] == $username ){
                     return true;
@@ -70,5 +112,7 @@
                 }
             }
         }
+
+
     }
 ?>
